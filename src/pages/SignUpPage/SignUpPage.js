f33e31b3ac6603ca8631c3395229bb8f3318f8d8
hgalvao98 <henrique.galvao@hotmail.com>
2021-06-28 16:23:30 -0300

    assets

--- src/pages/SignUpPage/SignUpPage.js
+++ src/pages/SignUpPage/SignUpPage.js
@@ -1,10 +1,26 @@
+import TextField from '@material-ui/core/TextField';
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import { Main } from './style'
+import logo from '../../assets/logo.png'
+
 
 const SignUpPage = () => {
     return (
-        <div>
-            SignUpPage
-        </div>
+        <Main>
+            <img src={logo}/>
+            <p>Cadastrar</p>
+            <form>
+            <TextField label="Nome*" placeholder="Nome e sobrenome" variant="outlined" />
+            <TextField label="E-mail*" placeholder="email@email.com" variant="outlined" />
+            <TextField label="CPF*" placeholder="000.000.000-00" variant="outlined" />
+            <TextField label="Senha*" placeholder="Mínimo 6 caracteres" variant="outlined" />
+            <TextField label="Confirmar*" placeholder="Confirme a senha anterior" variant="outlined" />
+            <Button variant="contained" color="primary">
+                Criar
+            </Button>
+            </form>
+        </Main>
     );
 };
 
---
+++ src/pages/SignUpPage/style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Main = styled.div`
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    justify-content:center;
+    form{
+        display:flex;
+        flex-direction:column;
+        align-items:center;
+    }
+`
+
