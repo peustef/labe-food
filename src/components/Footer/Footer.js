5a7324f50720951e836ad457d738edb0afd3290a
peustef <peustefanis@gmail.com>
2021-06-29 13:44:46 -0300

    footer funcionando parcialmente

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,30 +2,12 @@ import React, { useState } from 'react';
 import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
 import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
-import styled from 'styled-components';
-import { useHistory } from 'react-router';
-import { IconButton } from '@material-ui/core';
+import { goToChart, goToHome } from '../../routes/coordinator';
+import { StickFooter, StyledIconButton } from './style';
 
-const StickFooter = styled.div`
-overflow: hidden;
-position: fixed;
-display: flex;
-justify-content: space-around;
-bottom: 0;
-width: 100%;
-border-top: 1px solid #b8b8b8;
-background-color: #FFFFFF;
-`
 
-const StyledIconButton = styled(IconButton)`
-     &:hover{
-    color: #E0212C;
-  }
-`
-
-const Footer = () => {
+const Footer = ({ history }) => {
     const [value, setValue] = useState('recents');
-    const history = useHistory
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
@@ -36,10 +18,10 @@ const Footer = () => {
     return (
         <div>
             <StickFooter>
-                <StyledIconButton color="secondary" aria-label="open drawer">
+                <StyledIconButton color="secondary" aria-label="open drawer" onClick={() => goToHome(history)}>
                     <HomeOutlinedIcon fontSize="large" />
                 </StyledIconButton>
-                <StyledIconButton color="secondary">
+                <StyledIconButton color="secondary" onClick={() => goToChart(history)}>
                     <ShoppingCartOutlinedIcon fontSize="large" />
                 </StyledIconButton>
                 <StyledIconButton color="secondary" >
--- src/pages/ChartPage/ChartPage.js
+++ src/pages/ChartPage/ChartPage.js
@@ -2,24 +2,31 @@ import { Typography } from '@material-ui/core';
 import React from 'react';
 import { ContainerAdress, ContainerFood, ContainerInfoCart } from './style';
 import Payment from '../../components/Payment/Payment'
+import { useHistory } from 'react-router-dom';
+import Footer from '../../components/Footer/Footer';
 
 const ChartPage = () => {
+    const history = useHistory()
     return (
-        <containerCart>
-            <ContainerAdress>
-                <Typography variant={'body1'} color={'secondary'}>Endereço de Entrega</Typography>
-                <Typography variant={'body1'} >Rua Alessandra Vieira, 42</Typography>
-            </ContainerAdress>
-            <ContainerInfoCart>
-                <Typography variant={'body1'} color={'primary'} >Bullguer Vila Madalena</Typography>
-                <Typography variant={'body1'} color={'secondary'} >R. Fradique Coutinho, 1136 - Vila Madalena</Typography>
-                <Typography variant={'body1'} color={'secondary'} >30 - 45 min</Typography>
-            </ContainerInfoCart>
-            <ContainerFood>
-                <Typography variant={'body1'}  >Carrinho Vazio</Typography>
-            </ContainerFood>
-            <Payment />
-        </containerCart>
+        <div>
+
+            <containerCart>
+                <ContainerAdress>
+                    <Typography variant={'body1'} color={'secondary'}>Endereço de Entrega</Typography>
+                    <Typography variant={'body1'} >Rua Alessandra Vieira, 42</Typography>
+                </ContainerAdress>
+                <ContainerInfoCart>
+                    <Typography variant={'body1'} color={'primary'} >Bullguer Vila Madalena</Typography>
+                    <Typography variant={'body1'} color={'secondary'} >R. Fradique Coutinho, 1136 - Vila Madalena</Typography>
+                    <Typography variant={'body1'} color={'secondary'} >30 - 45 min</Typography>
+                </ContainerInfoCart>
+                <ContainerFood>
+                    <Typography variant={'body1'}  >Carrinho Vazio</Typography>
+                </ContainerFood>
+                <Payment />
+            </containerCart>
+            <Footer history={history} />
+        </div>
     );
 };
 
