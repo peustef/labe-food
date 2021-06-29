08929b20bc736a9b9fd29a34d259cf4465500026
hgalvao98 <henrique.galvao@hotmail.com>
2021-06-29 10:38:16 -0300

    faltando endpoint

--- src/pages/SignUpPage/SignUpPage.js
+++ src/pages/SignUpPage/SignUpPage.js
@@ -1,24 +1,86 @@
-import TextField from '@material-ui/core/TextField';
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import { Main } from './style'
+import { Main, Grid } from './style'
 import logo from '../../assets/logo.png'
+import TextField from '@material-ui/core/TextField';
+import { useHistory } from 'react-router-dom';
+import useForm from '../../hooks/useForm'
+
 
 
 const SignUpPage = () => {
+
+    const history = useHistory()
+    const [form, onChange, clear] = useForm({ name: "", email: "", cpf: "", password: "" })
+
+    const onSubmitForm = (event) => {
+        event.preventDefault()
+    }
+
     return (
         <Main>
-            <img src={logo}/>
-            <p>Cadastrar</p>
-            <form>
-            <TextField label="Nome*" placeholder="Nome e sobrenome" variant="outlined" />
-            <TextField label="E-mail*" placeholder="email@email.com" variant="outlined" />
-            <TextField label="CPF*" placeholder="000.000.000-00" variant="outlined" />
-            <TextField label="Senha*" placeholder="Mínimo 6 caracteres" variant="outlined" />
-            <TextField label="Confirmar*" placeholder="Confirme a senha anterior" variant="outlined" />
-            <Button variant="contained" color="primary">
-                Criar
-            </Button>
+            <img src={logo} />
+            <strong><p>Cadastrar</p></strong>
+            <form onSubmit={onSubmitForm}>
+                <Grid item>
+                    <TextField
+                        type='text'
+                        name={"name"}
+                        value={form.name}
+                        onChange={onChange}
+                        required
+                        label="Nome"
+                        placeholder="Nome e sobrenome"
+                        variant="outlined" />
+                </Grid>
+                <Grid item>
+                    <TextField
+                        type='email'
+                        name={"email"}
+                        value={form.email}
+                        onChange={onChange}
+                        required
+                        label="E-mail"
+                        placeholder="email@email.com"
+                        variant="outlined" />
+                </Grid>
+                <Grid item>
+                    <TextField
+                        name={"cpf"}
+                        value={form.cpf}
+                        onChange={onChange}
+                        required
+                        label="CPF"
+                        placeholder="000.000.000-00"
+                        variant="outlined" />
+                </Grid>
+                <Grid item>
+                    <TextField
+                        type='password'
+                        name={"password"}
+                        value={form.password}
+                        onChange={onChange}
+                        required
+                        label="Senha"
+                        placeholder="Mínimo 6 caracteres"
+                        variant="outlined" />
+                </Grid>
+                <Grid item>
+                    <TextField
+                        type='password'
+                        name
+                        value
+                        onChange
+                        required
+                        label="Confirmar"
+                        placeholder="Confirme a senha anterior"
+                        variant="outlined" />
+                </Grid>
+                <Button
+                    variant="contained"
+                    color="primary">
+                    Criar
+                </Button>
             </form>
         </Main>
     );
--- src/pages/SignUpPage/style.js
+++ src/pages/SignUpPage/style.js
@@ -1,14 +1,34 @@
 import styled from "styled-components";
 
-export const Main = styled.div`
-    display:flex;
-    flex-direction:column;
-    align-items:center;
-    justify-content:center;
-    form{
-        display:flex;
-        flex-direction:column;
-        align-items:center;
-    }
+
+export const Grid = styled.div`
+    margin:0.5em 0;
 `
+
+export const Main = styled.div`
+  font-family:'Roboto';
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+  justify-content:center;
+  img{
+      margin: 1.5rem 0;
+  }
+  form{
+      width:100%;
+      display:flex;
+      justify-content:center;
+      flex-direction:column;
+      align-items:center;
+      input{
+          width:20.5rem;
+      }
+      button{
+          width:22.5rem;
+          height:3.5rem;
+      }
+  }
+`
+
+
 
