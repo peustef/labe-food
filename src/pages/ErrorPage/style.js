7b60deb1cb0acf0d2bd53ef53bd016a7e058ff9f
peustef <peustefanis@gmail.com>
2021-07-01 10:03:19 -0300

    página de erro com botao para home

---
+++ src/pages/ErrorPage/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { Button } from '@material-ui/core';
+
+export const StyledButton = styled(Button)`
+    margin-top: 1rem;
+    padding: 1rem;
+`
+
+export const StyledImg = styled.img`
+    width: 9rem;
+`
